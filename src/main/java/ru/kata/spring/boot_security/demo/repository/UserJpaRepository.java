a2068b908ace5f0458692e88f9a1fd6c630adc60
salik2010 <salomonsalik1993@mail.ru>
2023-04-23 21:34:48 +0700

    1.1 dev

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserJpaRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserJpaRepository.java
@@ -14,6 +14,6 @@ public interface UserJpaRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     User findUserByUsername(String name);
 
-    Optional<User> findUserByEmail(String email);
+    //Optional<User> findUserByEmail(String email);
 
 }
